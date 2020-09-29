ab696e6d9894d8a7e2d559bb33ff2cfa765a8930
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-09-29 15:26:02 +0530

    datatypes and concat

---
+++ day2/datatypes.c
@@ -0,0 +1,15 @@
+// C program to illustrate strings 
+
+#include<stdio.h> 
+
+int main() { 
+
+    // Strings
+
+	char a[] = "Ujjwal"; 
+    char b[] = "Is"; 
+    char c[] = "Awesome"; 
+	
+	printf("String Concat Example: %s %s %s\n", a, b, c);
+
+} 
